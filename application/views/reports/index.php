50c5cd40ec2eb5f6be6dd2d2647213999a4c76d7
sleepinglion <admin@sleepinglion.pe.kr>
2016-12-27 17:37:32 +0900

    update

--- application/views/reports/index.php
+++
@@ -1,8 +0,0 @@
-<section id="sl_report_index">
-	<h3>주문내역</h3>
-	<?php if($data['total']): ?>
-	<?php foreach($data['list'] as $index=>$value): ?>
-	<?php echo anchor('reports/'.$value['id'],$value['id']) ?></td>
-	<?php endforeach ?>	
-	<?php endif ?>
-</section>
