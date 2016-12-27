a575ea3a15a3abe26e0fa63abd6194464818ebf7
sleepinglion <admin@sleepinglion.pe.kr>
2016-12-27 15:03:39 +0900

    update

--- application/models/Report.php
+++ application/models/Report.php
@@ -16,15 +16,15 @@ class Report extends CI_Model {
 	}
 
 	public function get_content($order_id) {
-		$this -> pdo -> where(array('reports.order_id' => $order_id,'reports.enable' => TRUE,'gene_categories.enable'=>TRUE,'report_categories.enable'=>TRUE));
-		$this -> pdo -> join('gene_categories','reports.id = gene_categories.id');
+		$this -> pdo -> where(array('reports.order_id' => $order_id,'reports.enable' => TRUE,'genes_related_actions.enable'=>TRUE,'report_categories.enable'=>TRUE));	
+		$this -> pdo -> join('genes_related_actions','reports.genes_related_action_id = genes_related_actions.id');
 		$this -> pdo -> join('report_categories','reports.id = report_categories.id');
 		
 		$result['total'] = $this -> pdo -> count_all_results('reports');
 	
-		$this -> pdo -> select('gene_categories.title as gene_category_title,report_categories.id as report_category_id,report_categories.title as report_category_title,reports.*');
+		$this -> pdo -> select('related_actions.title as gene_category_title,report_categories.id as report_category_id,report_categories.title as report_category_title,reports.*');
 		$this -> pdo -> where(array('reports.order_id' => $order_id,'reports.enable' => TRUE,'gene_categories.enable'=>TRUE,'report_categories.enable'=>TRUE));
-		$this -> pdo -> join('gene_categories','reports.gene_category_id = gene_categories.id');
+		$this -> pdo -> join('genes_related_actions','reports.genes_related_action_id = genes_related_actions.id');
 		$this -> pdo -> join('report_categories','reports.report_category_id = report_categories.id');
 		
 		$query = $this -> pdo -> get('reports');
