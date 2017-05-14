457163f1c93d3bd13f96db7afd7c59bd39b7940a
sleeping-lion <toughjjh@gmail.com>
2017-05-15 01:23:14 +0900

    수정함:        application/language/ko_KR/LC_MESSAGES/messages.mo 	수정함:        application/language/ko_KR/LC_MESSAGES/messages.po 	수정함:        application/views/home/index.php 	수정함:        application/views/layout/elements/footer.php 	수정함:        public/css/index.css

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,15 +1,15 @@
 <section id="home">
-	<h3>서비스 카피</h3>
+	<h2>서비스 카피</h2>
 	<article>
-		<h4>서비스 이미지</h4>
+		<h3>서비스 이미지</h3>
 	</article>
 	<article>
-		<h4>서비스 설명</h4>
+		<h3>서비스 설명</h3>
 	</article>
 	<article>
-		<h4>회사 설명</h4>
+		<h3>회사 설명</h3>
 	</article>
 	<article>
-		<h4>기타 설명</h4>
+		<h3>기타 설명</h3>
 	</article>
 </section>
--- application/views/layout/elements/footer.php
+++ application/views/layout/elements/footer.php
@@ -7,7 +7,7 @@
 			<option value="english"<?php if($this->session->userdata('locale')=='english'): ?> selected="selected"<?php endif ?>><?php echo _('english') ?></option>
 			<option value="korean"<?php if($this->session->userdata('locale')=='korean'): ?> selected="selected"<?php endif ?>><?php echo _('korean') ?></option>
 		</select>
-		<input type="submit" class="btn" value="<?php echo _('submit') ?>" />
+		<input type="submit" class="btn" value="<?php echo _('Submit') ?>" />
 	</form>
  	<script>
  		var base_url='<?php echo base_url() ?>';
