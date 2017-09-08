5d08267a050ef726850e5f299772a3ae8055dfd2
sleepinglion <admin@sleepinglion.pe.kr>
2017-09-08 18:29:30 +0900

    modified:   application/controllers/SL.php 	modified:   application/views/layout/default.php

--- application/controllers/SL.php
+++ application/controllers/SL.php
@@ -340,36 +340,6 @@ class SL_Controller extends CI_Controller
         $config['use_page_numbers'] = true;
         $config['query_string_segment'] = 'page';
 
-        $config['full_tag_open'] = '<div class="text-center"><ul class="pagination">';
-        $config['full_tag_close'] = '</ul></div><!--pagination-->';
-
-        $config['first_link'] = '&laquo; First';
-        $config['first_tag_open'] = '<li class="prev page">';
-        $config['first_tag_close'] = '</li>';
-
-        $config['last_link'] = 'Last &raquo;';
-        $config['last_tag_open'] = '<li class="next page">';
-        $config['last_tag_close'] = '</li>';
-
-        $config['next_link'] = 'Next &rarr;';
-        $config['next_tag_open'] = '<li class="next page">';
-        $config['next_tag_close'] = '</li>';
-
-        $config['prev_link'] = '&larr; Previous';
-        $config['prev_tag_open'] = '<li class="prev page">';
-        $config['prev_tag_close'] = '</li>';
-
-        $config['cur_tag_open'] = '<li class="active"><a href="">';
-        $config['cur_tag_close'] = '</a></li>';
-
-        $config['num_tag_open'] = '<li class="page">';
-        $config['num_tag_close'] = '</li>';
-        $config['next_link'] = '▶';
-        $config['prev_link'] = '◀';
-        // $config['display_pages'] = FALSE;
-        //
-        $config['anchor_class'] = 'follow_link';
-
         $query_string = $_GET;
         if (isset($query_string['page'])) {
             unset($query_string['page']);
@@ -379,6 +349,7 @@ class SL_Controller extends CI_Controller
             $config['suffix'] = '&' . http_build_query($query_string, '', "&");
             $config['first_url'] = $config['base_url'] . '?' . http_build_query($query_string, '', "&");
         }
+
         $this -> pagination -> initialize($config);
     }
 }
--- application/views/layout/default.php
+++ application/views/layout/default.php
@@ -3,28 +3,24 @@
 <head>
 	<?php echo meta($meta); ?>
 	<title><?php echo $title_for_layout; ?></title>
-	<link href="<?php echo base_url() ?>images/favicon.ico" type="image/x-icon" rel="shortcut icon"/>	
+	<link href="<?php echo base_url() ?>images/favicon.ico" type="image/x-icon" rel="shortcut icon"/>
 	<?php echo $style_for_layout ?>
 <?php meta_tags(array('general' => true,'og' => true,'twitter'=> true,'robot'=> true),$common_data['meta_title'],$common_data['meta_description']) ?>
 	<?php if(isset($data['tags'])): ?>
 	<meta content="<?php echo tag_restore($data['tags']) ?>" name="keywords" />
 	<?php else: ?>
-	<meta content="유전자,유전자검사,암,질병,비만" name="keywords" />
+	<meta content="shopping,payment" name="keywords" />
 	<?php endif ?>
 	<meta content="width=device-width, initial-scale=1.0" name="viewport" />
 	<meta name="author" content="Sleeping-Lion" />
 	<!--[if IE]>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 	<![endif]-->
-	<!--[if lt IE 9]>
-	<script type="text/javascript" src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-    <script type="text/javascript" src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
-    <![endif]-->
 </head>
 <body>
 	<?php echo $Layout->element('header') ?>
 	<div id="mom">
-		<div id="main" class="container">					
+		<div id="main" class="container">
 		<?php if($this -> router -> fetch_class()=='home'): ?>
 		<div id="main_main">
 			<?php echo $contents_for_layout ?>
@@ -38,7 +34,7 @@
 		</div>
 	</div>
 	<?php echo $Layout->element('footer') ?>
-	<div class="modal fade" id="myModal" tabindex="-1" role="dialog"></div>	
+	<div class="modal fade" id="myModal" tabindex="-1" role="dialog"></div>
 	<?php echo "\n".$script_for_layout."\n" ?>
 </body>
 </html>
