814f88329055f331aced5401ccca8abfd59b05e9
sleepinglion <admin@sleepinglion.pe.kr>
2017-09-08 18:29:53 +0900

    new file:   application/config/pagination.php

---
+++ application/config/pagination.php
@@ -0,0 +1,31 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * Pagination Config
+ *
+ * Just applying codeigniter's standard pagination config with twitter
+ * bootstrap stylings
+ *
+ * @license		http://www.apache.org/licenses/LICENSE-2.0  Apache License 2.0
+ * @author		Mike Funk
+ * @link		http://codeigniter.com/user_guide/libraries/pagination.html
+ * @email		mike@mikefunk.com
+ *
+ * @file		pagination.php
+ * @version		1.3.1
+ * @date		03/12/2012
+ *
+ * Copyright (c) 2011
+ */
+
+// --------------------------------------------------------------------------
+// $config['base_url'] = '';
+
+// $config['display_pages'] = FALSE;
+//
+
+// $config['display_pages'] = FALSE;
+//
+
+// --------------------------------------------------------------------------
+/* End of file pagination.php */
+/* Location: ./bookymark/application/config/pagination.php */
